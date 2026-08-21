16335924f66b4b15f0bf88f627695157ff1c0dff
velaHere <velahere7777@gmail.com>
2026-08-21 15:05:15 +0530

    Fixed health check controller

--- src/main/java/com/vela/velaCMS/controller/HealthController.java
+++ src/main/java/com/vela/velaCMS/controller/HealthController.java
@@ -2,9 +2,11 @@ package com.vela.velaCMS.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/healthy-vela")
+@RestController()
+@RequestMapping("/healthy-vela")
 public class HealthController {
     @GetMapping()
     public ResponseEntity<?> healthCheck() {
